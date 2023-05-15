5d4ee357aad353d7d40308a38feccfb083f25f0e
Lily <2475165+TsukihanaNo@users.noreply.github.com>
2023-05-15 01:01:10 -0400

    exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text, count) {
+    if(count>=0){
+        return text.repeat(count)        
+    }else{
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
