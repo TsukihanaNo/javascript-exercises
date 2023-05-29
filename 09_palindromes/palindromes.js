53df5ca02112a73cef076d2d64238d7ebe8b4aa9
Lily <2475165+TsukihanaNo@users.noreply.github.com>
2023-05-29 17:55:35 -0400

    exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const newString = words.replace(/\W+/g,"");
+    let finalString = "";
+    for (let i = newString.length-1;i>=0;i--){
+        finalString += newString[i];
+    }
+    console.log(newString);
+    return newString.toLowerCase()==finalString.toLowerCase();
 };
 
 // Do not edit below this line
